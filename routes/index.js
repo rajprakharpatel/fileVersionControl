5c6cd358c573a58dac0a4a6bc4f5672d7ce3bf55
rajprakhar <rajprakhar2000@gmail.com>
2021-11-02 13:04:18 +0530

    fix(bug): path resolution

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path')
-const axios = require('axios').default;
-const connectDB = require('../server/database/connection');
+var path = require("path");
+const axios = require("axios").default;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	// mongodb connection
-	const connection = connectDB();
-	if (connection) {
-		console.log('Hello ');
-	}
-	res.sendFile(path.resolve('./public/html/dashboard.html'), { title: 'Express' });
+router.get("/", function (req, res, next) {
+
+  res.sendFile(path.resolve(__dirname+"/../public/html/dashboard.html"), {
+    title: "Express",
+  });
 });
 
 module.exports = router;
